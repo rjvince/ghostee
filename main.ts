1619be5d0ffda1508f871dbc60ff74918a92dc15
rjvince <rob.vince@gmail.com>
2021-01-04 23:48:52 -0500

    Extra Lives, Sound, Fix Drawing Glitches

--- main.ts
+++ main.ts
@@ -1,19 +1,32 @@
 namespace SpriteKind {
     export const Enemy_MacPain = SpriteKind.create()
+    export const ExtraLife = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, myTiles.tile3, function (sprite, location) {
+    Ghostee.destroy()
+    for (let value of Enemies) {
+        value.destroy()
+    }
     Level += 1
-    loadLevel(Level)
+    loadLevel()
 })
 function handleDamage (enemy: Sprite) {
     turnEnemy(enemy)
     info.changeLifeBy(-1)
+    music.pewPew.play()
     if (info.life() > 0) {
         tiles.placeOnRandomTile(Ghostee, myTiles.tile2)
         Ghostee.startEffect(effects.halo, 500)
     }
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.ExtraLife, function (sprite, otherSprite) {
+    info.changeLifeBy(1)
+    otherSprite.destroy()
+    Ghostee.say("sweet!", 1000)
+    music.baDing.play()
+})
 function initBaddeeProperties (sprite: Sprite, _type: string) {
+    sprite.setFlag(SpriteFlag.BounceOnWall, false)
     sprite.ay = 500
     if (_type == "Mac") {
         sprites.setDataBoolean(sprite, "jumper", false)
@@ -36,11 +49,11 @@ function setClimbing (on: boolean) {
         Ghostee.ay = 500
     }
 }
-function loadLevel (num: number) {
-    if (num == 1) {
+function loadLevel () {
+    if (Level == 1) {
         tiles.setTilemap(tilemap`level_0`)
-    } else if (num == 2) {
-        tiles.setTilemap(tiles.createTilemap(hex`1e001600010000000000000000000000000000000000000000000000000000000001010200000000000000000000000000000000000000000000000000000001010100000000000400000000000000000000040000000000000000000001010101010105010101010101010501010101010101010501010101010101010000000005000000000000000500000000000000000500000000000001010006000005000000000000000500000000000000000500000000000001010101010101010501010101010101010101010105010101010101010101010000000000000500000000000000000000000005000000000000000001010000000400000500000000000000000004000005000000000000000001010101010101010101010101010501010101010101010101010101010101010000000000000000000000000500000000000000000000000000000001010000000000000006000000000500000000000000000000000000000001010101010101050101010101010101010101010105010101010101010101010000000000050000000000000000000000000005000000000000000001010000000000050000000000000000000004000005000000000000000001010101010101010101010501010101010101010101010101050101010101010000000000000000000500000000000000000000000000050000000001010000000400000000000500000000000000000000000400050000000001010101010101010101010101010105010101010101010101010101010101010000000000000000000000000005000000000000000000000000000001010000000000000000000000000005000000000600000000000000000301010101010101010101010101010101010101010101010101010101010101`, img`
+    } else if (Level == 2) {
+        tiles.setTilemap(tiles.createTilemap(hex`1e001600010000000000000000000000000000000000000000000000000000000001010200000000000000000000000000000000000000000000000000000001010100000000000400000000000000000000040000000000000000000001010101010105010101010101010501010101010101010501010101010101010000000005000000000000000500000000000000000500000000000001010006000005000000000000000500000000000000000500000000000001010101010101010501010101010101010101010105010101010101010101010000000000000500000000000000000000000005000000000000000001010000000400000500000000000000000004000005000000000000000001010101010101010101010101010501010101010101010101010101010101010000000000000000000000000500000000000000000000000000000001010000000000000006000000000500000000000000000007000000000001010101010101050101010101010101010101010105010101010101010101010000000000050000000000000000000000000005000000000000000001010000000000050000000000000000000004000005000000000000000001010101010101010101010501010101010101010101010101050101010101010000000000000000000500000000000000000000000000050000000001010000000400000000000500000000000000000000000400050000000001010101010101010101010101010105010101010101010101010101010101010000000000000000000000000005000000000000000000000000000001010000000000000000000000000005000000000600000000000000000301010101010101010101010101010101010101010101010101010101010101`, img`
             2............................2
             2............................2
             22...........................2
@@ -63,10 +76,103 @@ function loadLevel (num: number) {
             2............................2
             2............................2
             222222222222222222222222222222
-            `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile11], TileScale.Sixteen))
+            `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile11,myTiles.tile12], TileScale.Sixteen))
     } else {
         game.over(true, effects.smiles)
     }
+    for (let value of tiles.getTilesByType(myTiles.tile2)) {
+        Ghostee = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . 6 6 6 6 6 6 6 6 . . . . 
+            . . 6 6 9 9 9 9 9 9 9 9 6 6 . . 
+            . 6 9 9 9 9 9 9 9 9 9 9 9 9 6 . 
+            . 6 9 c c 1 9 9 9 9 c c 1 9 6 . 
+            . 6 9 c c 1 9 9 9 9 c c 1 9 6 . 
+            . 6 9 c c c 9 9 9 9 c c c 9 6 . 
+            . 6 9 1 c c 9 9 9 9 1 c c 9 6 . 
+            . 6 9 9 9 9 9 9 9 9 9 9 9 9 6 . 
+            . 6 9 6 9 9 9 6 6 9 9 9 6 9 6 . 
+            . 6 9 9 6 6 6 b b 6 6 6 9 9 6 . 
+            . 6 9 9 c b b b b b b c 9 9 6 . 
+            . 6 9 9 9 c b b b b c 9 9 9 6 . 
+            . 6 6 9 9 9 c c c c 9 9 9 6 6 . 
+            . . 6 6 6 6 6 6 6 6 6 6 6 6 . . 
+            . . . 6 6 . . 6 6 . . 6 6 . . . 
+            `, SpriteKind.Player)
+        tiles.placeOnTile(Ghostee, value)
+        scene.cameraFollowSprite(Ghostee)
+        Ghostee.ay = 10
+        Ghostee.setFlag(SpriteFlag.BounceOnWall, false)
+    }
+    for (let value of tiles.getTilesByType(myTiles.tile4)) {
+        Baddee = sprites.create(img`
+            . e e e e e e e . . . . . . . . 
+            e 4 4 4 4 4 4 e e e e . . . . . 
+            e 4 4 4 e e 4 4 4 4 e e e . . . 
+            e 4 4 4 4 e e 4 4 4 4 4 e e . . 
+            e 4 4 4 4 4 e e 4 4 4 4 4 e e . 
+            e 4 4 4 e e e e 4 4 4 4 4 4 e . 
+            e e 4 4 4 4 4 4 4 4 4 e e e e e 
+            . e 4 4 4 4 4 4 d d e 4 4 4 4 e 
+            . e 4 4 4 4 4 4 d e 4 4 4 4 4 e 
+            . e 4 4 4 4 4 4 4 e 4 4 4 4 4 e 
+            . e 4 4 4 4 4 4 e 4 4 4 4 4 4 e 
+            . e 4 4 4 4 4 4 4 4 4 4 4 4 4 e 
+            . . e 4 4 4 4 4 4 4 4 4 4 4 e . 
+            . . e 4 4 4 4 4 4 4 4 4 4 4 e . 
+            . . . e e 4 4 4 4 4 4 4 e e . . 
+            . . . . . e e e e e e e . . . . 
+            `, SpriteKind.Enemy)
+        tiles.placeOnTile(Baddee, value)
+        Baddee.image.flipX()
+        initBaddeeProperties(Baddee, "Mac")
+    }
+    for (let value of tiles.getTilesByType(myTiles.tile11)) {
+        Baddee = sprites.create(img`
+            . . . 4 4 4 9 9 9 9 . . . . . . 
+            . . 4 4 4 9 9 9 9 9 . . . . . . 
+            . . 4 4 4 9 9 9 9 9 9 . . . . . 
+            . 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
+            . 4 4 4 4 4 e e 4 4 4 4 4 4 4 . 
+            . b b d d d d e e d b b b . . . 
+            b d d b d d d d d d d d b b . . 
+            b d d d d d d d d d d d d b . . 
+            b d d d d d d d d d d d d b . . 
+            . b b d d d d d b d d d d b . . 
+            . b d d d d d d d b b b b b . . 
+            . . b d d d d d d e e e e e . . 
+            . . . b b b d d e e e e e e e . 
+            . . 4 4 4 4 b b e 4 e 4 e . e . 
+            . 4 4 4 4 4 . . . 4 4 4 4 4 . . 
+            4 4 4 4 4 4 . . . 4 4 4 4 4 4 . 
+            `, SpriteKind.Enemy)
+        tiles.placeOnTile(Baddee, value)
+        Baddee.image.flipX()
+        initBaddeeProperties(Baddee, "Sig")
+    }
+    for (let value of tiles.getTilesByType(myTiles.tile12)) {
+        oneUp = sprites.create(img`
+            . . . . . . . 3 a . . . . . . . 
+            . . . . . . 3 a a a . . . . . . 
+            . . . . . 3 3 . . a a . . . . . 
+            . . . . . 3 . . . . a . . . . . 
+            . . . . . 3 . . . . a . . . . . 
+            . . . . . 3 . . . . a . . . . . 
+            . . . . . a a . . a a . . . . . 
+            . . 3 a . . a a a a . . 3 a . . 
+            . . 3 a a 3 3 a a a a a a a . . 
+            . . . a . . . 3 a . . . a . . . 
+            . . . . . . . 3 a . . . . . . . 
+            . . . . . . . 3 a . . . . . . . 
+            . . . . . . . 3 a . . . . . . . 
+            . . . . . . . 3 a . . . . . . . 
+            . . . . . . . 3 a . . . . . . . 
+            . . . . . 3 a a a a a . . . . . 
+            `, SpriteKind.ExtraLife)
+        tiles.placeOnTile(oneUp, value)
+    }
+    Enemies = sprites.allOfKind(SpriteKind.Enemy)
+    music.jumpUp.play()
 }
 scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
     if (!(sprite.isHittingTile(CollisionDirection.Bottom))) {
@@ -90,81 +196,15 @@ function turnEnemy (sprite: Sprite) {
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     handleDamage(otherSprite)
 })
-let Ghostee: Sprite = null
+let oneUp: Sprite = null
 let Baddee: Sprite = null
+let Enemies: Sprite[] = []
+let Ghostee: Sprite = null
 let Level = 0
-Level = 1
-loadLevel(Level)
-for (let value of tiles.getTilesByType(myTiles.tile4)) {
-    Baddee = sprites.create(img`
-        . e e e e e e e . . . . . . . . 
-        e 4 4 4 4 4 4 e e e e . . . . . 
-        e 4 4 4 e e 4 4 4 4 e e e . . . 
-        e 4 4 4 4 e e 4 4 4 4 4 e e . . 
-        e 4 4 4 4 4 e e 4 4 4 4 4 e e . 
-        e 4 4 4 e e e e 4 4 4 4 4 4 e . 
-        e e 4 4 4 4 4 4 4 4 4 e e e e e 
-        . e 4 4 4 4 4 4 d d e 4 4 4 4 e 
-        . e 4 4 4 4 4 4 d e 4 4 4 4 4 e 
-        . e 4 4 4 4 4 4 4 e 4 4 4 4 4 e 
-        . e 4 4 4 4 4 4 e 4 4 4 4 4 4 e 
-        . e 4 4 4 4 4 4 4 4 4 4 4 4 4 e 
-        . . e 4 4 4 4 4 4 4 4 4 4 4 e . 
-        . . e 4 4 4 4 4 4 4 4 4 4 4 e . 
-        . . . e e 4 4 4 4 4 4 4 e e . . 
-        . . . . . e e e e e e e . . . . 
-        `, SpriteKind.Enemy)
-    tiles.placeOnTile(Baddee, value)
-    Baddee.image.flipX()
-    initBaddeeProperties(Baddee, "Mac")
-}
-for (let value of tiles.getTilesByType(myTiles.tile11)) {
-    Baddee = sprites.create(img`
-        . . . 4 4 4 9 9 9 9 . . . . . . 
-        . . 4 4 4 9 9 9 9 9 . . . . . . 
-        . . 4 4 4 9 9 9 9 9 9 . . . . . 
-        . 4 4 4 4 4 4 4 4 4 4 4 4 4 . . 
-        . 4 4 4 4 4 e e 4 4 4 4 4 4 4 . 
-        . b b d d d d e e d b b b . . . 
-        b d d b d d d d d d d d b b . . 
-        b d d d d d d d d d d d d b . . 
-        b d d d d d d d d d d d d b . . 
-        . b b d d d d d b d d d d b . . 
-        . b d d d d d d d b b b b b . . 
-        . . b d d d d d d e e e e e . . 
-        . . . b b b d d e e e e e e e . 
-        . . 4 4 4 4 b b e 4 e 4 e . e . 
-        . 4 4 4 4 4 . . . 4 4 4 4 4 . . 
-        4 4 4 4 4 4 . . . 4 4 4 4 4 4 . 
-        `, SpriteKind.Enemy)
-    tiles.placeOnTile(Baddee, value)
-    Baddee.image.flipX()
-    initBaddeeProperties(Baddee, "Sig")
-}
-let Enemies = sprites.allOfKind(SpriteKind.Enemy)
+music.setVolume(35)
 info.setLife(3)
-Ghostee = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . 6 6 6 6 6 6 6 6 . . . . 
-    . . 6 6 9 9 9 9 9 9 9 9 6 6 . . 
-    . 6 9 9 9 9 9 9 9 9 9 9 9 9 6 . 
-    . 6 9 c c 1 9 9 9 9 c c 1 9 6 . 
-    . 6 9 c c 1 9 9 9 9 c c 1 9 6 . 
-    . 6 9 c c c 9 9 9 9 c c c 9 6 . 
-    . 6 9 1 c c 9 9 9 9 1 c c 9 6 . 
-    . 6 9 9 9 9 9 9 9 9 9 9 9 9 6 . 
-    . 6 9 6 9 9 9 6 6 9 9 9 6 9 6 . 
-    . 6 9 9 6 6 6 b b 6 6 6 9 9 6 . 
-    . 6 9 9 c b b b b b b c 9 9 6 . 
-    . 6 9 9 9 c b b b b c 9 9 9 6 . 
-    . 6 6 9 9 9 c c c c 9 9 9 6 6 . 
-    . . 6 6 6 6 6 6 6 6 6 6 6 6 . . 
-    . . . 6 6 . . 6 6 . . 6 6 . . . 
-    `, SpriteKind.Player)
-Ghostee.setFlag(SpriteFlag.BounceOnWall, false)
-Ghostee.ay = 500
-tiles.placeOnRandomTile(Ghostee, myTiles.tile2)
-scene.cameraFollowSprite(Ghostee)
+Level = 1
+loadLevel()
 game.onUpdate(function () {
     if (onLadder() == 1) {
         setClimbing(true)
